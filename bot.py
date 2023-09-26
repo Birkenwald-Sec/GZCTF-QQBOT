cd352752a687cd5d5f8ed7a97d999134cf5584dc
l1Akr <2539439359@qq.com>
2023-09-26 15:05:09 +0800

    v/1.3

--- bot.py
+++ bot.py
@@ -1,21 +1,18 @@
 import nonebot
 from nonebot.adapters.cqhttp import Adapter
 
+
+
 nonebot.init()
 nonebot.init(apscheduler_autostart=True)
 nonebot.init(apscheduler_config={
     "apscheduler.timezone": "Asia/Shanghai"
 })
 
-
 driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 
-
-nonebot.load_builtin_plugins("echo")  # 内置插件
-# nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
-nonebot.load_plugins("src/plugins")  # 本地插件
-
+nonebot.load_plugins("src/plugins")
 
 if __name__ == "__main__":
     nonebot.run()
