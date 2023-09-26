64b2bd244df445fee9f39507cf1c382c362abff7
l1Akr <2539439359@qq.com>
2023-09-26 14:44:23 +0800

    v/1.3

--- src/plugins/gzctf-plugins/config.py
+++ src/plugins/gzctf-plugins/config.py
@@ -6,16 +6,16 @@ class Config(BaseModel, extra=Extra.ignore):
         # WHITE_LIST Must Be Set, Which Definds The Origin Of The Request
         # The id in the group must exist in the form of a string
         "WHITE_LIST" : { # Set Your List
-            "2539439359": {"type": "private"}
+            
         },
         # ENDPOINT Must Be Set, Which Definds The Response Goes Where
         "ENDPOINT" : { # Set Your Point
-            "user_id": ["2539439359"]
+            
         },
         # BASEURL Must Be Set, The Base Url Of Your Gzctf Platform, Like "https://192.168.0.1/"
-        "BASEURL": "https://www.birkenwald-lab.top/",
+        "BASEURL": "",
         # BC_FRESH_TIME Is Optional, Default 20
-        "BC_FRESH_TIME" : 5,
+        "BC_FRESH_TIME" : 20,
         # GAMEMONITORED Is Optional, Default [], Which Represents Monitoring Of All Games Which Is Opening
         "GAMEMONITORED" : [],
         # BC_MESSAGE_TEMPLATE Is Optional, Default "类型：{type} 于 {game_title}\n内容： {content}\n时间： {month}-{day} {time}"
